4ed8b7fd95bc7404edbbac4d0bf64855ea551cf1
abstractlabs <abstractlabs@gmail.com>
2015-12-13 20:07:59 -0700

    Added unit tests for parsing and regular expressions`

---
+++ ExtendType.OfString.Tests/Parsing/ExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ExtendType.OfString.Parsing;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace ExtendType.OfString.Tests.Parsing
+{
+	[TestClass]
+	public class ExtensionsTests
+	{
+		public TestContext TestContext
+		{
+			get;
+			set;
+		}
+
+		[TestMethod]
+		public void ToNumericTest()
+		{
+			const string control = "1234565";
+			string experemental = "A1b2.3$4%5+65";
+			((string)null).ToNumeric().Should().Be(null);
+			String.Empty.ToNumeric().Should().Be(String.Empty);
+			experemental.ToNumeric().Should().Be(control);
+		}
+
+		[TestMethod]
+		public void ToAlphabeticTest()
+		{
+			const string control = "Abcd";
+			string experemental = "A1b2.3c4%5d+65";
+			((string)null).ToAlphabetic().Should().Be(null);
+			String.Empty.ToAlphabetic().Should().Be(String.Empty);
+			experemental.ToAlphabetic().Should().Be(control);
+		}
+
+		[TestMethod]
+		public void ToAlphaNumericTest()
+		{
+			const string control = "A1b23c45d65";
+			string experemental = "A1b2.3c4%5d+65";
+			((string)null).ToAlphaNumeric().Should().Be(null);
+			String.Empty.ToAlphaNumeric().Should().Be(String.Empty);
+			experemental.ToAlphaNumeric().Should().Be(control);
+		}
+	}
+}
---
+++ ExtendType.OfString.Tests/RegularExpressions/ExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ExtendType.OfString.RegularExpressions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ExtendType.OfString.Tests.RegularExpressions
+{
+	[TestClass]
+	public class ExtensionsTests
+	{
+		public TestContext TestContext
+		{
+			get;
+			set;
+		}
+
+		[TestMethod]
+		public void ToRegexTest()
+		{
+			string experemental = @"023\d+";
+			((string)null).ToRegex().Should().Be(null);
+			String.Empty.ToRegex().IsMatch("012346").Should().BeTrue();
+			experemental.ToRegex().IsMatch("023456").Should().BeTrue();
+			String.Empty.ToRegex(RegexOptions.IgnoreCase).IsMatch("012346").Should().BeTrue();
+			experemental.ToRegex(RegexOptions.IgnoreCase).IsMatch("023456").Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void ToWildcard()
+		{
+			string experemental = @"something.*";
+			((string)null).ToWildcard().Should().Be(null);
+			String.Empty.ToWildcard().IsMatch("something.txt").Should().BeFalse();
+			experemental.ToWildcard().IsMatch("something.txt").Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void MatchTest()
+		{
+			string match = "0123456";
+			string pattern = @"0123\d+";
+			match.Match(pattern).Success.Should().BeTrue();
+			match.Match(pattern,RegexOptions.IgnoreCase).Success.Should().BeTrue();
+		}
+		[TestMethod]
+		public void IsMatchTest()
+		{
+			string match = "0123456";
+			string pattern = @"0123\d+";
+			match.IsMatch(pattern).Should().BeTrue();
+			match.IsMatch(pattern, RegexOptions.IgnoreCase).Should().BeTrue();
+		}
+	}
+}
